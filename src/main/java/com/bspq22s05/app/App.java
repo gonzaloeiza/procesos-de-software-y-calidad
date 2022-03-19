d81e6e7095ce36430c38b40d0797c58bda9d8a11
Gonzalo Eizaguirre <gonzaloeizaguirre@opendeusto.es>
2022-03-19 11:23:05 +0100

    proyecto maven creado

---
+++ src/main/java/com/bspq22s05/app/App.java
@@ -0,0 +1,13 @@
+package com.bspq22s05.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/com/bspq22s05/app/AppTest.java
@@ -0,0 +1,20 @@
+package com.bspq22s05.app;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
