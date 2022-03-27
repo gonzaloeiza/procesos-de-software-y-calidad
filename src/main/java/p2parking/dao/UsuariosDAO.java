f507174ba4b86570eb0439f244a2b968525ae10d
Imanol Gutierrez <developer@imanol.org>
2022-03-27 01:21:13 +0100

    Estructura DAO de las alquiler

---
+++ src/main/java/p2parking/dao/UsuariosDAO.java
@@ -0,0 +1,27 @@
+package p2parking.dao;
+
+import p2parking.jdo.Usuario;
+
+import java.util.List;
+
+public class UsuariosDAO extends DataAccessObjectBase implements iAccesoObjeto<Usuario> {
+    @Override
+    public void save(Usuario object) {
+
+    }
+
+    @Override
+    public void delete(Usuario object) {
+
+    }
+
+    @Override
+    public List<Usuario> getAll() {
+        return null;
+    }
+
+    @Override
+    public Usuario find(String param) {
+        return null;
+    }
+}
