95086c8b57402c782bbce1e117dd98f58b57f45f
Imanol Gutierrez <developer@imanol.org>
2022-03-29 20:51:24 +0200

    Cambiado lo de los ejemplos

--- src/main/java/p2parking/jdo/Main.java
+++ src/main/java/p2parking/jdo/Main.java
@@ -17,60 +17,15 @@ Contributors:
 **********************************************************************/
 package p2parking.jdo;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-
-import p2parking.dao.UsuariosDAO;
+import p2parking.dao.InsertarEjemplos; 
 
 
 public class Main {
 	
     @SuppressWarnings("unchecked")
 	public static void main(String args[]) {
-    	System.out.println("Starting ....");
-        // Create a PersistenceManagerFactory for this datastore
-        
-
-        System.out.println("DataNucleus AccessPlatform with JDO");
-        System.out.println("===================================");
-
-        
-        Plaza p1 = new Plaza(12.2f, "", new ArrayList<String>());
-        Plaza p2 = new Plaza(12.2f, "", new ArrayList<String>());
-        Plaza p3 = new Plaza(12.2f, "", new ArrayList<String>());  
-        
-        
-        Usuario u1 = new Usuario("gonzalo", "gonzaloeizaguirre@opendeusto.es", "1234", "ss", new HashSet<Plaza>());
-        Usuario u2 = new Usuario("javier", "javier@opendeusto.es", "1234", "ss", new HashSet<Plaza>());
-        Usuario u3 = new Usuario("endika", "endika@opendeusto.es", "1234", "ss", new HashSet<Plaza>());
-        Usuario u4 = new Usuario("oier", "oier@opendeusto.es", "1234", "ss", new HashSet<Plaza>());
-
-        u1.getPlazas().add(p1);
-        u1.getPlazas().add(p2);
-        u2.getPlazas().add(p3);
-       
-        
-        UsuariosDAO.getInstance().save(u1);
-        UsuariosDAO.getInstance().save(u2);
-        UsuariosDAO.getInstance().save(u3);
-        UsuariosDAO.getInstance().save(u4);
-        
-        
-//        ArrayList<Plaza> listaPlazas1 = new ArrayList<Plaza>();
-//        listaPlazas1.add(p1);
-//        listaPlazas1.add(p2);
-//
-//        ArrayList<Plaza> listaPlazas2 = new ArrayList<Plaza>();
-//        listaPlazas1.add(p3);
-        
-        
-       
-//        
-//        
-
-        
-        
-        
-        System.out.println("");
+    	
+    	// insertar ejemplos desde la clase InsertarEjemplos del DAO
+    	InsertarEjemplos.insertar();
     }
 }
