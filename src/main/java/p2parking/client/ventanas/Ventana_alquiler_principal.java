e41b4555424174628b96578f1fc10d37f02d8e83
oierfer <oierfer@users.noreply.github.com>
2022-04-03 12:34:59 +0200

    idem

--- src/main/java/p2parking/client/ventanas/Ventana_alquiler_principal.java
+++ src/main/java/p2parking/client/ventanas/Ventana_alquiler_principal.java
@@ -32,7 +32,7 @@ public class Ventana_alquiler_principal extends JFrame {
 	public Ventana_alquiler_principal() {
 		setTitle("P2Parking");
     	setForeground(SystemColor.windowBorder);
-    	setIconImage(Toolkit.getDefaultToolkit().getImage(Ventana_alquiler_principal.class.getResource("/p2parking/client/ventanas/P2.jpg")));
+    	//setIconImage(Toolkit.getDefaultToolkit().getImage(Ventana_alquiler_principal.class.getResource("/p2parking/client/ventanas/P2.jpg")));
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 666, 428);
 		panelPricipal = new JPanel();
