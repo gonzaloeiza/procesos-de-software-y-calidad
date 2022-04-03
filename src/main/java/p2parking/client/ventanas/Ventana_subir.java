46209541bd2ecf70e5e8e6593fd13e005f8e03ce
Lander Martin <lander.martin@opendeusto.es>
2022-04-03 17:23:59 +0200

    Ventana Subir

---
+++ src/main/java/p2parking/client/ventanas/Ventana_subir.java
@@ -0,0 +1,180 @@
+package p2parking.client.ventanas;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+import java.awt.GridLayout;
+import java.awt.Rectangle;
+import java.awt.SystemColor;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JButton;
+import javax.swing.JFileChooser;
+import javax.swing.JTextField;
+
+public class Ventana_subir extends JFrame {
+	private JPanel panelPricipal;	
+	private JTextField textPrecio;
+	private JTextField textUbicacion;
+	
+	public Ventana_subir() {
+		setTitle("P2Parking");
+    	setForeground(SystemColor.windowBorder);
+//    	setIconImage(Toolkit.getDefaultToolkit().getImage(Inicio_sesion.class.getResource("/p2parking/client/ventanas/P2.jpg")));
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 666, 481);
+		panelPricipal = new JPanel();
+		panelPricipal.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(panelPricipal);
+		panelPricipal.setLayout(new BorderLayout(0, 0));
+		
+		JPanel panel_arriba = new JPanel();
+		panelPricipal.add(panel_arriba, BorderLayout.NORTH);
+		panel_arriba.setLayout(new GridLayout(1, 3, 0, 0));
+		
+		JButton perfil = new JButton("");
+		perfil.setBounds(new Rectangle(2, 2, 1, 1));
+		perfil.setBackground(SystemColor.activeCaptionBorder);
+		//perfil.setIcon(new ImageIcon(Ventana_alquiler_principal.class.getResource("/p2parking/client/ventanas/usuario3.jpg")));
+		panel_arriba.add(perfil);
+		
+		JLabel logo = new JLabel("");
+		//logo.setIcon(new ImageIcon(Ventana_alquiler_principal.class.getResource("/p2parking/client/ventanas/p2larfo.jpg")));
+		panel_arriba.add(logo);
+		
+		JPanel panel_arribaderecha = new JPanel();
+		panel_arriba.add(panel_arribaderecha);
+		panel_arribaderecha.setLayout(new GridLayout(2, 1, 0, 0));
+		
+		JPanel panel_abajo = new JPanel();
+		panelPricipal.add(panel_abajo, BorderLayout.SOUTH);
+		
+		JButton btnSubir = new JButton("Subir");
+		panel_abajo.add(btnSubir);
+		
+		JPanel panel_central = new JPanel();
+		panelPricipal.add(panel_central, BorderLayout.CENTER);
+		panel_central.setLayout(new GridLayout(7, 4, 0, 0));
+		
+		
+	
+		JPanel panel = new JPanel();
+		panel_central.add(panel);
+		
+	
+		
+		JPanel panel_1 = new JPanel();
+		panel_central.add(panel_1);
+		
+		JPanel panel_2 = new JPanel();
+		panel_central.add(panel_2);
+		
+		JPanel panel_3 = new JPanel();
+		panel_central.add(panel_3);
+		
+		textUbicacion = new JTextField();
+		textUbicacion.setText("Cual es la direccion?");
+		panel_central.add(textUbicacion);
+		textUbicacion.setColumns(10);
+		
+		JPanel panel_4 = new JPanel();
+		panel_central.add(panel_4);
+		
+		
+		JPanel panel_5 = new JPanel();
+		panel_central.add(panel_5);
+		
+		JPanel panel_6 = new JPanel();
+		panel_central.add(panel_6);
+		
+		JPanel panel_7 = new JPanel();
+		panel_central.add(panel_7);
+		
+		JPanel panel_8 = new JPanel();
+		panel_central.add(panel_8);
+		
+		textPrecio = new JTextField();
+		textPrecio.setText("Precio");
+		panel_central.add(textPrecio);
+		textPrecio.setColumns(10);
+		
+		JPanel panel_9 = new JPanel();
+		panel_central.add(panel_9);
+		
+		JPanel panel_10 = new JPanel();
+		panel_central.add(panel_10);
+		
+		JPanel panel_11 = new JPanel();
+		panel_central.add(panel_11);
+		
+		JPanel panel_12 = new JPanel();
+		panel_central.add(panel_12);
+		
+		JPanel panel_13 = new JPanel();
+		panel_central.add(panel_13);
+		
+		JButton btnNewButton = new JButton("Fotos de la Plaza");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				JFileChooser filechoser=new JFileChooser();
+				filechoser.showOpenDialog(panel_central);
+			}
+		});
+		panel_central.add(btnNewButton);
+		
+		JPanel panel_14 = new JPanel();
+		panel_central.add(panel_14);
+		
+		JPanel panel_15 = new JPanel();
+		panel_central.add(panel_15);
+		
+		JPanel panel_16 = new JPanel();
+		panel_central.add(panel_16);
+		
+		JPanel panel_17 = new JPanel();
+		panel_central.add(panel_17);
+		
+		
+	}
+
+	public static void main(String[] args) {
+		try {
+            // Set cross-platform Java L&F (also called "Metal")
+        UIManager.setLookAndFeel("com.jtattoo.plaf.aluminium.AluminiumLookAndFeel");
+	    } 
+	    catch (UnsupportedLookAndFeelException e) {
+	       // handle exception
+	    }
+	    catch (ClassNotFoundException e) {
+	       // handle exception
+	    }
+	    catch (InstantiationException e) {
+	       // handle exception
+	    }
+	    catch (IllegalAccessException e) {
+	       // handle exception
+	    }
+	    	
+	
+	
+    	
+        EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Inicio_sesion frame = new Inicio_sesion();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+    }
+
+
+}
