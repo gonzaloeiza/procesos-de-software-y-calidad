3ec9e8edff2a1806a97fc5a46d5ba954a4d98603
Gonzalo Eizaguirre <gonzaloeizaguirre@opendeusto.es>
2022-04-06 10:06:15 +0200

    update gitignore

--- bin/src/sql/create-p2parkingdb.sql
+++
@@ -1,7 +0,0 @@
-DROP SCHEMA IF EXISTS p2parkingDB;
-DROP USER IF EXISTS 'spq'@'localhost';
-
-CREATE SCHEMA p2parkingDB;
-CREATE USER IF NOT EXISTS 'spq'@'localhost' IDENTIFIED BY 'spq';
-
-GRANT ALL ON p2parkingDB.* TO 'spq'@'localhost';
