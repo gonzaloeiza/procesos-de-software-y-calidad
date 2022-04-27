a1159e02da2a00319d13e95b1004d2ffdbec1a93
oierfer <oierfer@users.noreply.github.com>
2022-04-27 10:32:02 +0200

    Creada estructura para tests

--- src/test/java/p2parking/dao/PlazasDAOTest.java
+++ src/test/java/p2parking/dao/PlazasDAOTest.java
@@ -1,1 +1,15 @@
 package p2parking.dao;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class PlazasDAOTest {
+	
+	@Before
+	public void setUp() {}
+	
+	@Test
+	public void testGetNombre() {}
+}
