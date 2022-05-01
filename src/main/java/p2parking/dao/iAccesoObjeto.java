5b41163edf8b54c6fbfce2e9916c1518aa26f5e9
oierfer <oierfer@users.noreply.github.com>
2022-05-01 18:26:57 +0200

    Cambiados metodos para que devuelvan boolean

--- src/main/java/p2parking/dao/iAccesoObjeto.java
+++ src/main/java/p2parking/dao/iAccesoObjeto.java
@@ -4,8 +4,8 @@ import java.util.List;
 
 
 public interface iAccesoObjeto<DomainObject> {
-    public void save(DomainObject object);
-    public void delete(DomainObject object);
+    public boolean save(DomainObject object);
+    public boolean delete(DomainObject object);
     public List<DomainObject> getAll();
     public DomainObject find(String param);
     public List<DomainObject> findAll(String param);
