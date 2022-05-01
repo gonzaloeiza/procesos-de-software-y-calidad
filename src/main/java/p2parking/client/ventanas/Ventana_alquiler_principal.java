1e2df1f98ea32796472c061cfab67a73ee9b6e53
Imanol Gutierrez <developer@imanol.org>
2022-05-01 20:26:15 +0200

    arreglar commit

--- src/main/java/p2parking/client/ventanas/Ventana_alquiler_principal.java
+++ src/main/java/p2parking/client/ventanas/Ventana_alquiler_principal.java
@@ -38,12 +38,17 @@ import java.awt.event.InputMethodEvent;
 public class Ventana_alquiler_principal extends JFrame {
 	private JPanel panelPricipal;
 	private ArrayList<Plaza> plazas;
+	private ArrayList<Plaza> plazasOrdenadas;
+	private boolean ordenado;
 	private ArrayList<Plaza> plazasFav;
 	private int indice=0;
 
 	
 	public Ventana_alquiler_principal() {
 		plazas= Remote.getInstance().getAllPlazas(Remote.getInstance().getToken());
+		plazasOrdenadas = new ArrayList<Plaza>();
+		ordenarPlazas();
+		ordenado = false;
 		plazasFav=Remote.getInstance().getMisFav(Remote.getInstance().getToken());
 		
 		setTitle("P2Parking");
@@ -173,11 +178,6 @@ public class Ventana_alquiler_principal extends JFrame {
 		panel_top.setLayout(new GridLayout(1, 6, 0, 0));
 		
 		JButton btnOrden = new JButton("Ordenar Plazas");
-		btnOrden.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				
-			}
-		});
 		panel_top.add(btnOrden);
 		
 		JPanel panel_4 = new JPanel();
@@ -407,22 +407,28 @@ public class Ventana_alquiler_principal extends JFrame {
 
 	}
 	
-	private void ordenarPlaza() {
-		int indexAtras;
+	// ordena la lista de menor a mayor
+	private void ordenarPlazas() {
+		float ultimoPrecio = (float) 0;
+		int index;
 		
-		if (plazas.size() < 2)
-			return;
+		ArrayList<Plaza> aux = plazas;
+		Plaza aMeter;
 		
-		for (int index=1; index<plazas.size(); index++) {
-			indexAtras = index;
-			if (plazas.get(index).getPrecio() < plazas.get(index - 1).getPrecio()) {
-				while (plazas.get(index).getPrecio() <  plazas.get(indexAtras).getPrecio()) {
-					indexAtras = indexAtras - 1;
+		while(plazas.size() > 0) {
+			index = 0;
+			aMeter = null;
+			while(index < plazas.size()) {
+				if (aMeter == null || plazas.get(index).getPrecio() >= ultimoPrecio) {
+					aMeter = plazas.get(index);
+					ultimoPrecio = aMeter.getPrecio();
 				}
-				plazas.add(indexAtras, plazas.get(index));
-				plazas.remove(index+1);
+				index++;
 			}
+			plazasOrdenadas.add(aMeter);
+			aux.remove(index);
 		}
+		
 	}
 
 }
