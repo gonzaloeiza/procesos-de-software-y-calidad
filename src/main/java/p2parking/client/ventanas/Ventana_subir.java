f84481a664080f1748b33d074029816b81fa7db6
Gonzalo Eizaguirre <gonzaloeizaguirre@opendeusto.es>
2022-05-03 15:51:17 +0200

    comentar fotos en ventana subir plaza porque no funciona

--- src/main/java/p2parking/client/ventanas/Ventana_subir.java
+++ src/main/java/p2parking/client/ventanas/Ventana_subir.java
@@ -48,7 +48,7 @@ public class Ventana_subir extends JFrame {
 	public Ventana_subir() {
 		setTitle("P2Parking");
     	setForeground(SystemColor.windowBorder);
-    	setIconImage(Toolkit.getDefaultToolkit().getImage(Inicio_sesion.class.getResource("/p2parking/client/ventanas/P2.jpg")));
+//    	setIconImage(Toolkit.getDefaultToolkit().getImage(Inicio_sesion.class.getResource("/p2parking/client/ventanas/P2.jpg")));
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 666, 481);
 		panelPricipal = new JPanel();
@@ -75,7 +75,7 @@ public class Ventana_subir extends JFrame {
 		panel_arribaderecha.setLayout(new GridLayout(2, 1, 0, 0));
 		
 		JLabel logo = new JLabel("");
-		logo.setIcon(new ImageIcon(Ventana_alquiler_principal.class.getResource("/p2parking/client/ventanas/p2larfo.jpg")));
+//		logo.setIcon(new ImageIcon(Ventana_alquiler_principal.class.getResource("/p2parking/client/ventanas/p2larfo.jpg")));
 		panel_arriba.add(logo);
 		
 
