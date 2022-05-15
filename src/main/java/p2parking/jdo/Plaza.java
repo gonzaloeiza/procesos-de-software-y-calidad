d6357864e67d5f510d81f297194cc57d49708ab1
Imanol Gutierrez <developer@imanol.org>
2022-05-15 18:58:50 +0200

    JavaDoc de la Plaza

--- src/main/java/p2parking/jdo/Plaza.java
+++ src/main/java/p2parking/jdo/Plaza.java
@@ -1,7 +1,6 @@
 package p2parking.jdo;
 
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
 import javax.jdo.annotations.Element;
@@ -9,6 +8,11 @@ import javax.jdo.annotations.Join;
 import javax.jdo.annotations.PersistenceCapable;
 import javax.jdo.annotations.Persistent;
 
+/**
+ * Clase de una plaza de parking
+ * @author imanol
+ *
+ */
 @PersistenceCapable
 public class Plaza {
 
@@ -26,6 +30,13 @@ public class Plaza {
     private String descripcion;
     private boolean seguro;
 	
+    /**
+     * Constructor de la clase plaza
+     * @param precio precio de alquiler por hora
+     * @param localizacion localización de la plaza
+     * @param fotos foto de la plaza
+     * @param fechaPublicacion fecha en la que se ha publicado la plaza
+     */
 	public Plaza(float precio, String localizacion, ArrayList<String> fotos, long fechaPublicacion) {
 		this.precioDia = precio;
 		this.localizacion = localizacion;
@@ -33,6 +44,16 @@ public class Plaza {
 		this.fechaPublicacion = fechaPublicacion;
 	}
 
+	/**
+	 * Constructor de la plaza 
+	 * @param precioDia precio de alquiler de la plaza por dia
+	 * @param localizacion localizacion de la plaza
+	 * @param fotos fotos de la plaza
+	 * @param fechaPublicacion fecha de publicación de la plaza
+	 * @param titulo titulo de la plaza
+	 * @param descripcion texto explicativo de la plaza
+	 * @param seguro identificador de si la plaza cuenta con seguro o no
+	 */
 	public Plaza(float precioDia, String localizacion, ArrayList<String> fotos, long fechaPublicacion,
 			 String titulo, String descripcion, boolean seguro) {
 		super();
@@ -45,74 +66,146 @@ public class Plaza {
 		this.seguro = seguro;
 	}
 
+	/**
+	 * Geter del precio de alquiler
+	 * @return
+	 */
 	public float getPrecio() {
 		return precioDia;
 	}
 
+	/**
+	 * Seter del precio por hora de la plaza
+	 * @param precio cantidad que cuesta la plaza por hora
+	 */
 	public void setPrecio(float precio) {
 		this.precioDia = precio;
 	}
 
+	/**
+	 * Geter de la localización de la plaza
+	 * @return localización de la plaza
+	 */
 	public String getLocalizacion() {
 		return localizacion;
 	}
 
+	/**
+	 * Seter de la localizacion de la plaza
+	 * @param localizacion nueva localizacion de la plaza
+	 */
 	public void setLocalizacion(String localizacion) {
 		this.localizacion = localizacion;
 	}
 
+	/**
+	 * Geter de las fotos de la plaza
+	 * @return arraylist de fotos de las plazas
+	 */
 	public ArrayList<String> getFotos() {
 		return fotos;
 	}
 
+	/**
+	 * Seter de las fotos de la plaza
+	 * @param fotos lista de las fotos de las plzas
+	 */
 	public void setFotos(ArrayList<String> fotos) {
 		this.fotos = fotos;
 	}
 
+	/**
+	 * Geter de la fecha de publicación de la plaza
+	 * @return fecha de publicación de la plaza
+	 */
 	public long getFechaPublicacion() {
 		return fechaPublicacion;
 	}
 
+	/**
+	 * Seter de la fecha de publicación de la plaza
+	 * @param fechaPublicacion nueva fecha de publicacion
+	 */
 	public void setFechaPublicacion(long fechaPublicacion) {
 		this.fechaPublicacion = fechaPublicacion;
 	}
 
+	/**
+	 * Usuairo propietario de la plaza
+	 * @return usuario propietario
+	 */
 	public Usuario getPropietario() {
 		return propietario;
 	}
 
+	/**
+	 * Seter del usuario propietario de la plaza
+	 * @param propietario nuevo propietario de la plaza
+	 */
 	public void setPropietario(Usuario propietario) {
 		this.propietario = propietario;
 	}
 
+	/**
+	 * Geter de los usuarios que tienen esta plaza como favorita
+	 * @return lista de usuarios favoritos
+	 */
 	public List<Usuario> getUsuariosFavoritos() {
 		return usuariosFavoritos;
 	}
 
+	/**
+	 * Seter de los usuarios favoritos de la plaza
+	 * @param usuariosFavoritos nueva lista de los usuarios favoritos
+	 */
 	public void setUsuariosFavoritos(List<Usuario> usuariosFavoritos) {
 		this.usuariosFavoritos = usuariosFavoritos;
 	}
 
+	/**
+	 * Geter de la descipcion de la plaza
+	 * @return texto descriptivo de la plaza
+	 */
 	public String getDescripcion() {
 		return descripcion;
 	}
 
+	/**
+	 * Seter de la nueva descripcion de la plaza
+	 * @param descripcion de la plaza
+	 */
 	public void setDescripcion(String descripcion) {
 		this.descripcion = descripcion;
 	}
 
+	/**
+	 * Geter del titulo de la plaza
+	 * @return texto del titulo de la plaza
+	 */
 	public String getTitulo() {
 		return titulo;
 	}
 
+	/**
+	 * Seter del nuevo titulo de la plaza
+	 * @param titulo nuevo titulo
+	 */
 	public void setTitulo(String titulo) {
 		this.titulo = titulo;
 	}
-
+	
+	/**
+	 * Geter de la variable seguro de la plaza
+	 * @return devuelve true si tiene seguro y false si no tiene seguro
+	 */
 	public boolean isSeguro() {
 		return seguro;
 	}
 
+	/**
+	 * Seter de si tiene seguro la plaza
+	 * @param seguro estado de si tiene seguro la plaza
+	 */
 	public void setSeguro(boolean seguro) {
 		this.seguro = seguro;
 	}
