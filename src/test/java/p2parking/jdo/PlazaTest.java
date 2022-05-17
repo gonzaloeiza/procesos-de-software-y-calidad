98d8d73424a8c5215cf11a443ea828972cf0ae01
Imanol Gutierrez <developer@imanol.org>
2022-05-17 10:50:26 +0200

    Test carga de Plaza

--- src/test/java/p2parking/jdo/PlazaTest.java
+++ src/test/java/p2parking/jdo/PlazaTest.java
@@ -7,10 +7,19 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
+import org.databene.contiperf.PerfTest;
+import org.databene.contiperf.Required;
+import org.databene.contiperf.junit.ContiPerfRule;
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
 
+@PerfTest(invocations = 5)
+@Required(max = 1200, average = 250)
 public class PlazaTest {
+	
+	@Rule
+	public ContiPerfRule rule = new ContiPerfRule();
 	
 	private Plaza plaza1;
 	private Plaza plaza2;
