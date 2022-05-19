4b2b72d4cf4bd2e7f7eb050a7f8cb183718e0bf2
Lander Martin <lander.martin@opendeusto.es>
2022-05-19 20:18:11 +0200

    Javadoc

--- src/main/java/p2parking/client/ventanas/funcionalidad/Ventana_subir_funcionalidad.java
+++ src/main/java/p2parking/client/ventanas/funcionalidad/Ventana_subir_funcionalidad.java
@@ -10,14 +10,37 @@ import p2parking.client.ventanas.Ventana_alquiler_principal;
 import p2parking.client.ventanas.Ventana_subir;
 import p2parking.jdo.Plaza;
 
+/**
+ * 
+ * @author lander
+ *
+ */
 public class Ventana_subir_funcionalidad {
 	
+	/**
+	 * 
+	 * @param vent La ventana en la que se muestra
+	 */
 	public static void botonVolver(Ventana_subir vent) {
 		Ventana_alquiler_principal.main(null);
 		vent.dispose();
 		
 	}
 	
+	/**
+	 * 
+	 * @param elm1 Grados c1
+	 * @param elm2 Minutois C1
+	 * @param elm3 segundos C1
+	 * @param elm4 Grados c2
+	 * @param elm5 minutos C2
+	 * @param elm7 segundos c2
+	 * @param titulo Titulo de la plaza
+	 * @param descripcion Descripcion de la plaza
+	 * @param seguro True or false si tiene seguro
+	 * @param precio Precion de la plaza
+	 * @param vent La ventana en la que se muestra
+	 */
 	public static void botonSubir(String elm1, String elm2, String elm3, String elm4, String elm5,String elm7,String titulo,
 			String descripcion,boolean seguro,String precio,Ventana_subir vent) {
 		String cordenada1= elm1+"º"+elm2+"'"+elm1+"\"N";
@@ -45,6 +68,11 @@ public class Ventana_subir_funcionalidad {
 		
 	}
 	
+	/**
+	 * 
+	 * @param seguro Variable si tiene seguro o no
+	 * @return Valor si tiene seguro o no
+	 */
 	public static boolean checkBoxSeguro(boolean seguro) {
 		if(seguro==true) {
 			seguro=false;
