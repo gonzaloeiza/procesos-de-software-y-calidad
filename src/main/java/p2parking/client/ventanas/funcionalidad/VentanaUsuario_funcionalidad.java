28e392097e306af89a188ef3052e19fd0eb968d3
Lander Martin <lander.martin@opendeusto.es>
2022-05-19 20:18:22 +0200

    Javadoc

--- src/main/java/p2parking/client/ventanas/funcionalidad/VentanaUsuario_funcionalidad.java
+++ src/main/java/p2parking/client/ventanas/funcionalidad/VentanaUsuario_funcionalidad.java
@@ -3,6 +3,11 @@ package p2parking.client.ventanas.funcionalidad;
 import p2parking.client.ventanas.VentanaUsuario;
 import p2parking.client.ventanas.Ventana_alquiler_principal;
 
+/**
+ * 
+ * @author lander
+ *
+ */
 public class VentanaUsuario_funcionalidad {
 	public static void botonVolver(VentanaUsuario vent) {
 		Ventana_alquiler_principal.main(null);
