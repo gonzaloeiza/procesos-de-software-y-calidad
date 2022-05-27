863264736a97109f33c7bcd08a0b21d7bf324349
Javier Contreras <javicoa@opendeusto.es>
2022-05-27 10:37:42 +0200

    nueva ventana para admin

---
+++ src/main/java/p2parking/client/ventanas/VentanaAdminPrincipal.java
@@ -0,0 +1,44 @@
+package p2parking.client.ventanas;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import java.awt.Font;
+
+public class VentanaAdminPrincipal extends JFrame {
+	
+	private static VentanaAdminPrincipal frame;
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					frame = new VentanaAdminPrincipal();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+
+	}
+
+	
+	public VentanaAdminPrincipal() {
+		getContentPane().setLayout(null);
+		
+		JLabel lblListadoDeUsuarios = new JLabel("Listado de Usuarios");
+		lblListadoDeUsuarios.setFont(new Font("Tahoma", Font.BOLD, 13));
+		lblListadoDeUsuarios.setBounds(250, 74, 165, 16);
+		getContentPane().add(lblListadoDeUsuarios);
+		setTitle("P2Parking");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 666, 481);
+	}
+	
+	
+}
--- src/main/java/p2parking/client/ventanas/funcionalidad/VentanaAdminLogInFuncionalidad.java
+++ src/main/java/p2parking/client/ventanas/funcionalidad/VentanaAdminLogInFuncionalidad.java
@@ -3,7 +3,9 @@ package p2parking.client.ventanas.funcionalidad;
 import javax.swing.JOptionPane;
 
 import p2parking.client.Remote;
+import p2parking.client.ventanas.Inicio_sesion;
 import p2parking.client.ventanas.VentanaAdminLogIn;
+import p2parking.client.ventanas.VentanaAdminPrincipal;
 
 public class VentanaAdminLogInFuncionalidad {
 
@@ -11,8 +13,12 @@ public class VentanaAdminLogInFuncionalidad {
             System.out.println(password);
             if (Remote.getInstance().adminLogin(Remote.constructorRequest(password))) {
                 System.out.println("login correcto");
+                VentanaAdminPrincipal.main(null);
+                vent.dispose();
             } else {
                 JOptionPane.showMessageDialog(null, "No se ha podido realizar el inicio de sesion");
             }
         }
+    
+    
 }
